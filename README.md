0d1060a850e8e56ba1eea308f7df010f3b785fcb
James Tan <jamesruperttan@gmail.com>
2024-10-26 20:55:21 -0400

    Setup SolidJS

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # solitaire
+A simple solitaire suite
+
+# Features
+- [ ] Free-cell
+- [ ] Klondike
+- [ ] Gameplay
+    - [ ] High scores
+        - Personal
+        - Global
+    - [ ] Achievements
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [ nodejs_22 ];
+    };
+  };
+}
